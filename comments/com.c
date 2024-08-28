cf190154941170dc8f6e13d785bcf3b762e2fa43
PihooGokhle <pihoogokhle@gmail.com>
2024-08-28 20:30:26 +0530

    add com

---
+++ comments/com.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    // this dollar var is for dollar sign not a value
+    // char dollar = '$';
+ 
+    /*
+        int dollarValue = 10;
+        printf("%d", "I have only ", dollar );
+    */
+    
+}
