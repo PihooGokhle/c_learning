3652f71187d4870f6bfd5c447e54888385dc6ce6
PihooGokhle <pihoogokhle@gmail.com>
2024-09-16 23:31:35 +0530

    add arithpow.c file

--- instructions/arithpow.c
+++ instructions/arithpow.c
@@ -1,10 +1,14 @@
 # include<stdio.h>
-# include<math.h>
 
 int main(){
+
     int a,b;
-    a = 4;
-    b = 2;
-    printf("%d" , a/b );
+    a = 2;
+    b = 4;
+
+    int power = pow(b,a);
+
+    printf("%d", power);
+
     return 0;
 }
