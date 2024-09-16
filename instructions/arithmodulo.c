45f77effc8e83b0c0fd30c5d810b49dda84678b4
PihooGokhle <pihoogokhle@gmail.com>
2024-09-16 23:23:27 +0530

    add arithmodulo.c file

--- instructions/arithmodulo.c
+++ instructions/arithmodulo.c
@@ -2,9 +2,17 @@
 # include<math.h>
 
 int main(){
+
     int a,b;
-    a = 4;
-    b = 2;
-    printf("%d" , a/b );
+
+    printf("enter a");
+    scanf("%d",&a);
+
+    printf("enter b");
+    scanf("%d",&b);
+
+    printf("remainder is %d", a%b);
+
     return 0;
 }
+
