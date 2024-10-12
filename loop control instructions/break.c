da16d704b22e975dfdd47977f04d8781e14acec6
Ayush-G09 <ayushgokhle@gmail.com>
2024-10-12 23:26:35 +0530

    add break.c file

---
+++ loop control instructions/break.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main(){
+    
+    for(int i = 1 ; i <= 18 ; i++){
+        if(i == 12){
+            break;
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+    
+}
