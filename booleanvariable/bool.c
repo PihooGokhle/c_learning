8747b1d68052013cba427bc4a42d914544e81b33
PihooGokhle <pihoogokhle@gmail.com>
2025-02-17 21:28:52 +0530

    add bool.c file

---
+++ booleanvariable/bool.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# include<stdbool.h>
+
+int main(){
+
+    bool marks = true;
+    bool percentage = false;
+
+    printf("%d\n", marks);
+    printf("%d", percentage);
+
+    return 0;
+}
