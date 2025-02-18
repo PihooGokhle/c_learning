7bbbe14f0550a096e012278a44f7e98a2f57aec5
PihooGokhle <pihoogokhle@gmail.com>
2025-02-18 23:24:00 +0530

    add changeelement.c and loopinarray file

---
+++ arrays/changeelement.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+int main(){
+
+    int numbers[] = {20, 25, 30, 35, 40};
+    numbers[4] = 58;
+    
+    printf("%d", numbers[4]);
+
+    return 0;
+}
---
+++ arrays/loopinarray.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+
+    int age[] = {98, 54, 24};
+
+    for(int i = 0; i < 3; i++){
+        printf("%d\n", age[i]);
+    }
+
+    return 0;
+}
