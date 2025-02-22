844a10b3ea77e4b85df388e28ce94564ee01fa7c
PihooGokhle <pihoogokhle@gmail.com>
2025-02-23 00:17:42 +0530

    add struct.c file

--- structures/struct.c
+++ structures/struct.c
@@ -1,22 +1,20 @@
 # include<stdio.h>
 # include<string.h>
 
-struct student{
+struct member{
+
     int roll;
-    float cgpa;
     char name[100];
 };
 
 int main(){
-    struct student s1;
-    s1.roll = 2345;
-    s1.cgpa = 7.9;
-    strcpy(s1.name, "pihoo");
 
-    printf("student name = %s\n", s1.name);
-    printf("student roll no. = %d\n", s1.roll);
-    printf("student cgpa = %f\n", s1.cgpa);
+    struct member s1;
+    s1.roll = 54;
+    strcpy(s1.name, "Pihoo");
+
+    printf("Your roll no. is %d\n", s1.roll);
+    printf("Your roll no. is %s\n", s1.name);
 
     return 0;
-
 }
